f0b4765a836b26b7ed9514bcd266bfda9a7f1efb
GUNW-O-O <rhrjsdn12@naver.com>
2025-09-30 23:37:44 +0900

    ...

---
+++ basic/코드처리하기/Solution.java
@@ -0,0 +1,25 @@
+package 코드처리하기;
+
+public class Solution {
+  public String solution(String code) {
+    char[] arr = code.toCharArray();
+    StringBuilder sb = new StringBuilder();
+    int idx = 0;
+    int mode = 0;
+    for (char s : arr) {
+      if(mode == 0) {
+        if(arr[idx] == 39) mode = 1;
+        else if(idx % 2 == 0) {
+          sb.append(arr[idx]);
+        }
+      } else {
+        if(arr[idx] == 39) mode = 0;
+        else if(idx % 2 == 1) {
+          sb.append(arr[idx]);
+        }
+      }
+      idx++;
+    }
+    return (sb.length() == 0) ? "EMPTY" : sb.toString();
+  }
+}
