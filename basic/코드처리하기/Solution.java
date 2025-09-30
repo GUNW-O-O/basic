6b194ab9495cfc0f88205aca8ed65ccbfb70da4f
GUNW-O-O <rhrjsdn12@naver.com>
2025-09-30 23:45:52 +0900

    1 = 아스키코드 49

--- basic/코드처리하기/Solution.java
+++ basic/코드처리하기/Solution.java
@@ -6,15 +6,28 @@ public class Solution {
     StringBuilder sb = new StringBuilder();
     int idx = 0;
     int mode = 0;
+    // for (int i = 0; i < code.length(); i++) {
+    //   char current = code.charAt(i);
+    //   if (current == '1') {
+    //     mode = mode == 0 ? 1 : 0;
+    //     continue;
+    //   }
+
+    //   if (i % 2 == mode) {
+    //     answer.append(current);
+    //   }
+    // }
     for (char s : arr) {
-      if(mode == 0) {
-        if(arr[idx] == 49) mode = 1;
-        else if(idx % 2 == 0) {
+      if (mode == 0) {
+        if (arr[idx] == 49)
+          mode = 1;
+        else if (idx % 2 == 0) {
           sb.append(arr[idx]);
         }
       } else if (mode == 1) {
-        if(arr[idx] == 49) mode = 0;
-        else if(idx % 2 == 1) {
+        if (arr[idx] == 49)
+          mode = 0;
+        else if (idx % 2 == 1) {
           sb.append(arr[idx]);
         }
       }
