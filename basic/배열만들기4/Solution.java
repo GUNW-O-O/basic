6241140a1e8085c63f874dcbb3d2adfff0a4e489
GUNW-O-O <rhrjsdn12@naver.com>
2025-10-03 20:10:47 +0900

    제한시간 초과

---
+++ basic/배열만들기4/Solution.java
@@ -0,0 +1,28 @@
+package 배열만들기4;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+  public int[] solution(int[] arr) {
+    // 제한시간 초과;;
+    // List<Integer> list = new ArrayList<>();
+    // int idx = 0;
+    // while (idx < arr.length) {
+    //   if (list.size() == 0) {
+    //     list.add(arr[idx]);
+    //     idx++;
+    //   } else if (list.size() != 0 && list.get(list.size() - 1) < arr[idx]) {
+    //     list.add(arr[idx]);
+    //     idx++;
+    //   } else if (list.size() != 0 && list.get(list.size() - 1) > arr[idx]) {
+    //     list.remove(list.size() - 1);
+    //   }
+    // }
+    // int[] stk = new int[list.size()];
+    // for(int i = 0; i < list.size(); i++) {
+    //   stk[i] = list.get(i);
+    // }
+    // return stk;
+  }
+}
