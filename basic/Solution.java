b9a8dfa9b58a1d439f72092e11aa4f3f9c4f0195
GUNW-O-O <rhrjsdn12@naver.com>
2025-10-07 17:28:33 +0900

    중복검사 효율성 높여보기

--- basic/Solution.java
+++ basic/Solution.java
@@ -1,8 +1,20 @@
-import java.util.Arrays;
-
 class Solution {
   public int solution(int[] array) {
-    Arrays.sort(array);
-    return array[array.length / 2];
+    int[] u = new int[1000];
+    for (int i = 0; i < array.length; i++) {
+      u[array[i]]++;
+    }
+    int max = 0;
+    int idx = 0;
+    for (int i = 0; i < u.length; i++) {
+      if(u[i] > max) {
+        max = u[i];
+        idx = i;
+      }
+    }
+    for (int i = 0; i < u.length; i++) {
+      if(max == u[i] && idx != i) return -1;
+    }
+    return idx;
   }
 }
