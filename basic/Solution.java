542c4509ee065e7a7250843834ae171b2d397cc4
GUNW-O-O <rhrjsdn12@naver.com>
2025-10-07 17:32:23 +0900

    스트림

--- basic/Solution.java
+++ basic/Solution.java
@@ -1,20 +1,7 @@
+import java.util.stream.IntStream;
+
 class Solution {
-  public int solution(int[] array) {
-    int[] u = new int[1000];
-    for (int i = 0; i < array.length; i++) {
-      u[array[i]]++;
-    }
-    int max = 0;
-    int idx = 0;
-    for (int i = 0; i < u.length; i++) {
-      if(u[i] > max) {
-        max = u[i];
-        idx = i;
-      }
-    }
-    for (int i = 0; i < u.length; i++) {
-      if(max == u[i] && idx != i) return -1;
-    }
-    return idx;
+  public int[] solution(int n) {
+    return IntStream.rangeClosed(1, n).filter(i -> i%2 ==1).toArray();
   }
 }
