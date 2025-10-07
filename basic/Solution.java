5e256dbf163961b19c1eeb17f74297bd2f1d5562
GUNW-O-O <rhrjsdn12@naver.com>
2025-10-07 16:24:38 +0900

    파일정리, 유클리드 호제법 찾아보기

--- basic/Solution.java
+++ basic/Solution.java
@@ -1,16 +1,13 @@
-import java.util.ArrayList;
-import java.util.List;
-
 class Solution {
-  public int solution(String my_string, String is_suffix) {
-    List<String> list = new ArrayList<>();
-    for(int i = my_string.length(); i > 0; i--) {
-      list.add(my_string.substring(i-1, my_string.length()));
+  public int[] solution(int numer1, int denom1, int numer2, int denom2) {
+    int a1 = numer1 * denom2;
+    int a2 = numer2 * denom1;
+    int a = a1 + a2;
+    int b = denom1 * denom2;
+    if (a % 2 == 0 && b % 2 == 0) {
+      a /= 2;
+      b /= 2;
     }
-    for(String s : list) {
-      if(is_suffix.equals(s)) return 1;
-    }
-    return 0;
-    // return myString.endsWith(isSuffix) ? 1 : 0;
+    return new int[]{a, b};
   }
 }
