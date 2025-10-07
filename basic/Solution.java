3c92af956d6217aeab9a4dd67d2a1051cb4cb088
GUNW-O-O <rhrjsdn12@naver.com>
2025-10-07 16:28:38 +0900

    .

--- basic/Solution.java
+++ basic/Solution.java
@@ -1,15 +1,7 @@
+import java.util.Arrays;
+
 class Solution {
-  public int[] solution(int numer1, int denom1, int numer2, int denom2) {
-    int a1 = numer1 * denom2;
-    int a2 = numer2 * denom1;
-    int a = a1 + a2;
-    int b = denom1 * denom2;
-    for (int i = 999; i > 1; i--) {
-      if(a % i == 0 && b % i == 0) {
-        a /= i;
-        b /= i;
-      }
-    }
-    return new int[]{a, b};
+  public int[] solution(int[] numbers) {
+    return Arrays.stream(numbers).map(e -> e*2).toArray();
   }
 }
