da17fe31e117e841dd89af43a1917a286fd5971d
GUNW-O-O <rhrjsdn12@naver.com>
2025-10-08 06:27:44 +0900

    gcd

--- basic/Solution.java
+++ basic/Solution.java
@@ -1,7 +1,13 @@
 class Solution {
   public int solution(int n) {
-    double p = n / 7.0;
-    p = Math.ceil(p);
-    return (int)p;
+    int gcd = gcd(6, n);
+    int lcm = 6 * n / gcd;
+    return lcm / 6;
+  }
+
+  private int gcd(int a, int b) {
+    if (b == 0)
+      return a;
+    return gcd(b, a % b);
   }
 }
