a18ac88de419589d31c33c94eb6e87a29e9ef897
GUNW-O-O <rhrjsdn12@naver.com>
2025-10-11 19:53:26 +0900

    startsWith

--- basic/Solution.java
+++ basic/Solution.java
@@ -1,5 +1,5 @@
 class Solution {
-  public String solution(String my_string, int n) {
-    return my_string.substring(0, n);
+  public int solution(String my_string, String is_prefix) {
+    return my_string.startsWith(is_prefix) ? 1 : 0;
   }
 }
