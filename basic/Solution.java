bba9c967c53dbc5f9bde101cdaa8880d77fabe55
GUNW-O-O <rhrjsdn12@naver.com>
2025-10-11 20:22:23 +0900

    ..

--- basic/Solution.java
+++ basic/Solution.java
@@ -1,7 +1,9 @@
 class Solution {
-  public String solution(String my_string, int s, int e) {
-    StringBuilder sb = new StringBuilder(my_string.substring(s, e+1));
-    sb.reverse();
-    return my_string.replace(my_string.substring(s, e+1), sb);
+  public String solution(String my_string, int m, int c) {
+    StringBuilder sb = new StringBuilder();
+    for (int i = 0; i < my_string.length(); i += m) {
+      sb.append(my_string.charAt(i + c - 1));
+    }
+    return sb.toString();
   }
 }
