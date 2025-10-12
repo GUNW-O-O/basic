7481121e17e28029ce5a548b41e58a452563031f
GUNW-O-O <rhrjsdn12@naver.com>
2025-10-12 21:44:56 +0900

    ..

--- basic/Solution.java
+++ basic/Solution.java
@@ -1,14 +1,12 @@
 class Solution {
-  public int[] solution(int start_num, int end_num) {
-    int[] answer = new int[start_num - end_num + 1];
-    int idx = 0;
-    int num = start_num;
-    for (int i = 0; i < answer.length; i++) {
-      answer[idx] = num;
-      idx++;
-      num--;
+  public int solution(int[] arr, int idx) {
+    int answer = -1;
+    for (int i = idx; i < arr.length; i++) {
+      if (arr[i] == 1) {
+        answer = i;
+        break;
+      }
     }
     return answer;
-    // return IntStream.rangeClosed(-start, -end).map(i -> -i).toArray()
   }
 }
