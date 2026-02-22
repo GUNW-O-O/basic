318170b33d0754f74f9069340da89966f8cb3f0f
GUNW-O-O <rhrjsdn12@naver.com>
2026-02-22 17:51:55 +0900

    map

---
+++ basic/map/완주하지못한선수/Solution.java
@@ -0,0 +1,22 @@
+package map.완주하지못한선수;
+
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+  public String solution(String[] participant, String[] completion) {
+    Map<String, Integer> map = new HashMap<>();
+    for (String s : participant) {
+      map.put(s, map.getOrDefault(s, 0) + 1);
+    }
+    for (String s : completion) {
+      map.put(s, map.get(s) - 1);
+    }
+    for(String key : map.keySet()) {
+      if(map.get(key) > 0) {
+        return key;
+      }
+    }
+    return "";
+  }
+}
