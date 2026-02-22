43c41f7c08f71061c149f5b4bd93871c8bbe408f
GUNW-O-O <rhrjsdn12@naver.com>
2026-02-22 19:45:47 +0900

    기능개발

--- basic/stkque/기능개발/Solution.java
+++ basic/stkque/기능개발/Solution.java
@@ -1,13 +1,42 @@
 package stkque.기능개발;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
 
 class Solution {
-  boolean solution(String s) {
-    boolean answer = true;
+  public int[] solution(int[] progresses, int[] speeds) {
+    Deque<Integer> progress = new ArrayDeque<>();
+    Deque<Integer> speed = new ArrayDeque<>();
+    Deque<Integer> complete = new ArrayDeque<>();
 
-    // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-    System.out.println("Hello Java");
+    for (int i : progresses) {
+      progress.offerLast(i);
+    }
+    for (int i : speeds) {
+      speed.offerLast(i);
+    }
+    while (!progress.isEmpty()) {
+      int cnt = 0;
+      while (!progress.isEmpty() && progress.peekFirst() >= 100) {
+        progress.pollFirst();
+        speed.pollFirst();
+        cnt++;
+      }
+      for (int i = 0; i < progress.size(); i++) {
+        int spd = speed.pollFirst();
+        int cur = progress.pollFirst() + spd;
+        progress.offerLast(cur);
+        speed.offerLast(spd);
+      }
+      if(cnt > 0) {
+        complete.offerLast(cnt);
+      }
+    }
 
+    int[] answer = new int[complete.size()];
+    for (int i = 0; i < answer.length; i++) {
+      answer[i] = complete.pollFirst();
+    }
     return answer;
   }
 }
