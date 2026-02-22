1ac514d046b69e432753fc01c24c17afade67069
GUNW-O-O <rhrjsdn12@naver.com>
2026-02-22 18:17:01 +0900

    의상

--- basic/hash/의상/Solution.java
+++ basic/hash/의상/Solution.java
@@ -1,8 +1,18 @@
 package hash.의상;
 
+import java.util.HashMap;
+import java.util.Map;
+
 class Solution {
   public int solution(String[][] clothes) {
-    int answer = 0;
-    return answer;
+    Map<String, Integer> map = new HashMap<>();
+    int answer = 1;
+    for (String[] cloth : clothes) {
+      map.put(cloth[1], map.getOrDefault(cloth[1], 0) + 1);
+    }
+    for (int count : map.values()) {
+      answer *= (count + 1);
+    }
+    return answer-1;
   }
 }
