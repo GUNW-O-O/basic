5f3084e91fcd5441f25debc74d2103c12c6094a9
GUNW-O-O <rhrjsdn12@naver.com>
2026-02-24 17:07:30 +0900

    greedy

--- basic/Solution.java
+++ basic/Solution.java
@@ -1,12 +1,6 @@
 class Solution {
-  public int solution(int[] arr, int idx) {
-    int answer = -1;
-    for (int i = idx; i < arr.length; i++) {
-      if (arr[i] == 1) {
-        answer = i;
-        break;
-      }
-    }
+  public int[] solution(int[] order int target) {
+    int answer = 0;
     return answer;
   }
 }
---
+++ basic/greedy/큰수만들기/Solution.java
@@ -0,0 +1,17 @@
+package greedy.큰수만들기;
+
+class Solution {
+  public String solution(String number, int k) {
+    StringBuilder sb = new StringBuilder();
+    int remove = k;
+    for (char c : number.toCharArray()) {
+      while (sb.length() > 0 && remove > 0
+            && sb.charAt(sb.length()-1) < c) {
+        sb.deleteCharAt(sb.length()-1);
+        remove--;
+      }
+      sb.append(c);
+    }
+    return sb.substring(0, sb.length() - remove);
+  }
+}
