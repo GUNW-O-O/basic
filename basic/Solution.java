4e6ed942d62c08be1ed75d38f977b0ea66901ebe
GUNW-O-O <rhrjsdn12@naver.com>
2026-02-24 20:35:19 +0900

    leetcode - twosum

--- basic/Solution.java
+++ basic/Solution.java
@@ -1,6 +1,16 @@
+import java.util.HashMap;
+import java.util.Map;
+
 class Solution {
-  public int[] solution(int[] order int target) {
-    int answer = 0;
-    return answer;
+  public int[] solution(int[] nums, int target) {
+    Map<Integer, Integer> map = new HashMap<>();
+    for(int i = 0; i < nums.length; i++) {
+      int tar = target - nums[i];
+      if(map.containsKey(tar)) {
+        return new int[] {map.get(tar), i};
+      }
+      map.put(nums[i], i);
+    }
+    return new int [2];
   }
 }
